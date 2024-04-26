fe7417f2350da3d8c28b22efdb6ad4ed460b352f
DIVYAMORE <101706928+DIVYAMORE@users.noreply.github.com>
2024-04-27 00:33:25 +0530

    Add files via upload

---
+++ webview_java.java
@@ -0,0 +1,33 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) WebView webView=findViewById(R.id.Webview);
+        WebSettings webSettings=webView.getSettings();
+        webView.setWebViewClient(new WebViewClient());
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.pict.edu/");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        }
+        else
+            super.onBackPressed();
+    }
+}
